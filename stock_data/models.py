029b01dd75371216d25c1138b117aca412910cb5
AdamSzabo08 <szaboadam9@gmail.com>
2024-06-04 14:39:57 +0300

    Update

--- stock_data/models.py
+++ stock_data/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Stock(models.Model):
-    symbol = models.CharField(max_length=10)
+    symbol = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class StockPrice(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
@@ -12,3 +15,9 @@ class StockPrice(models.Model):
     high_price = models.FloatField()
     low_price = models.FloatField()
     volume = models.BigIntegerField()
+
+    class Meta:
+        unique_together = ('stock', 'date')
+
+    def __str__(self):
+        return f"{self.stock.symbol} on {self.date}"
--- stock_data/views.py
+++ stock_data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Avg
+from django.db.models import Avg, Max, Min, Sum
 from .models import Stock, StockPrice
 
 def stock_list(request):
@@ -9,9 +9,34 @@ def stock_list(request):
 def stock_detail(request, symbol):
     stock = get_object_or_404(Stock, symbol=symbol)
     stock_prices = StockPrice.objects.filter(stock=stock).order_by('-date')
-    return render(request, 'stock_data/stock_detail.html', {'stock': stock, 'stock_prices': stock_prices})
+    
+    dates = [price.date.strftime('%Y-%m-%d') for price in stock_prices]
+    close_prices = [price.close_price for price in stock_prices]
+    
+    context = {
+        'stock': stock,
+        'stock_prices': stock_prices,
+        'dates': dates,
+        'close_prices': close_prices
+    }
+    
+    return render(request, 'stock_data/stock_detail.html', context)
 
 def stock_analysis(request, symbol):
     stock = get_object_or_404(Stock, symbol=symbol)
-    average_close = StockPrice.objects.filter(stock=stock).aggregate(Avg('close_price'))['close_price__avg']
-    return render(request, 'stock_data/stock_analysis.html', {'stock': stock, 'average_close': average_close})
+    stock_prices = StockPrice.objects.filter(stock=stock)
+    
+    average_close = stock_prices.aggregate(Avg('close_price'))['close_price__avg']
+    highest_close = stock_prices.aggregate(Max('close_price'))['close_price__max']
+    lowest_close = stock_prices.aggregate(Min('close_price'))['close_price__min']
+    average_volume = stock_prices.aggregate(Avg('volume'))['volume__avg']
+    
+    context = {
+        'stock': stock,
+        'average_close': average_close,
+        'highest_close': highest_close,
+        'lowest_close': lowest_close,
+        'average_volume': average_volume
+    }
+    
+    return render(request, 'stock_data/stock_analysis.html', context)
